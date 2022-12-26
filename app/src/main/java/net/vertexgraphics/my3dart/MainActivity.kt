e6ed656af1fa60a8b7775477f0714aa173230d23
Predatorkind <l.trepinski@gmail.com>
2022-12-26 13:49:19 +0000

    initial commit

---
+++ app/src/main/java/net/vertexgraphics/my3dart/MainActivity.kt
@@ -0,0 +1,43 @@
+package net.vertexgraphics.my3dart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.vertexgraphics.my3dart.ui.theme.My3DArtTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            My3DArtTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    My3DArtTheme {
+        Greeting("Android")
+    }
+}
