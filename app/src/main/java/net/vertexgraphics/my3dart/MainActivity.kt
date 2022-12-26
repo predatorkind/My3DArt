b27b19cb9b7805dd924d44a76fc7d60a0261753e
Predatorkind <l.trepinski@gmail.com>
2022-12-26 19:39:42 +0000

    updated UI

--- app/src/main/java/net/vertexgraphics/my3dart/MainActivity.kt
+++ app/src/main/java/net/vertexgraphics/my3dart/MainActivity.kt
@@ -5,17 +5,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -54,11 +60,25 @@ fun MainScreen() {
             R.drawable.logo}
     }
     Column(modifier = Modifier
-        .padding(32.dp)
+        .padding(16.dp)
         .wrapContentSize(align = Alignment.Center)
         ) {
-        Image(painter = painterResource(id = image), contentDescription = image.toString(),
-            modifier = Modifier.size(400.dp))
+        Surface(modifier= Modifier
+            .padding(0.dp)
+            .shadow(
+                10.dp, RoundedCornerShape(5.dp), false, Color.Black,
+                spotColor = Color.Black
+            ),
+            shape = RoundedCornerShape(5.dp),
+            border = BorderStroke(1.dp, Color.Black),
+
+            color = Color(LocalContext.current.resources.getColor(R.color.green, LocalContext.current.theme))){
+            Image(painter = painterResource(id = image), contentDescription = image.toString(),
+                modifier = Modifier
+                    .size(400.dp)
+                    .padding(20.dp))
+        }
+        Spacer(modifier = Modifier.height(16.dp))
         Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()){
             ImageFlipButton(label = R.string.previous, value = currentImage, onClick = { if (currentImage > 1) currentImage--})
             ImageFlipButton(label = R.string.next, value = currentImage, onClick = { currentImage++})
